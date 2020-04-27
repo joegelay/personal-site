691f37e9daf2507030c36653ffdb1f82887aae95
joegelay <joegelay@gmail.com>
2020-04-27 11:36:55 -0600

    add profile picture

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
   return (
     <div className="App">
       <body>
+
       <h1>Hi, I'm Joe.</h1>
+      <img src="./images/profile-picture.jpg" alt="Joe Gelay" />
       <p>I love to learn new things, solve problems, and connect people. 
-        My background in marketing has shaped me into being a detail-oriented thinker 
-        that knows how to dissect big challenges into actionable objectives. 
+        My background in marketing analytics has shaped me into being a detail-oriented thinker 
+        with an eye for the big picture. 
         As a former Peace Corps Volunteer, I know how to work with ambiguity, 
         adapt to changes with grace, and communicate effectively to any team or audience. 
         When my eyes need a break from the screen, I like to climb rocks, run trails, and read books. </p>
