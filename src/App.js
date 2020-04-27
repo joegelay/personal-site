d53794a55a69f7b56eec1fa4807b7c4587848c92
joegelay <joegelay@gmail.com>
2020-04-27 13:52:28 -0600

    add animation to nav links

--- src/App.js
+++ src/App.js
@@ -8,16 +8,32 @@ function App() {
     <div className="App">
       <body>
 
-      <h1>Hi, I'm Joe.</h1>
-      <div id="image-cropper">
-        <img src={profilePicture} alt="Joe Gelay" id="profile-picture" />
-      </div>
-      <p>I love to learn new things, solve problems, and connect people. 
+      <nav class="main-nav">
+        <a class="main-nav-link" href="">Home</a>
+        <a class="main-nav-link" href="" href="">Projects</a>
+        <a class="main-nav-link" href="" href="">Resume</a>
+        <a class="main-nav-link" href="" href="">Writing</a>
+        <a class="main-nav-link" href="" href="">Reading</a>
+      </nav>
+
+      <header id="hi">Hi, I'm Joe.</header>
+
+
+      <div id="blurb-and-image">
+      <p>I love to learn things, solve problems, and connect people.
+        <br></br> 
+        <br></br> 
         My background in marketing analytics has shaped me into being a detail-oriented thinker 
         with an eye for the big picture. 
         As a former Peace Corps Volunteer, I know how to work with ambiguity, 
         adapt to changes with grace, and communicate effectively with any team or audience. 
+        <br></br> 
+        <br></br> 
         When my eyes need a break from the screen, I like to climb rocks, run trails, and read books. </p>
+      <div id="image-cropper">
+        <img src={profilePicture} alt="Joe Gelay" id="profile-picture" />
+      </div>
+      </div>
 
       </body>
     </div>
