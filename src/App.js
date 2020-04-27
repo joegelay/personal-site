357761f018bddb991d29972df1fdfae6c85c9bd7
joegelay <joegelay@gmail.com>
2020-04-27 10:59:34 -0600

    add profile picture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import './styles/reset.css';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
+      <body>
       <h1>Hi, I'm Joe.</h1>
+      <p>I love to learn new things, solve problems, and connect people. 
+        My background in marketing has shaped me into being a detail-oriented thinker 
+        that knows how to dissect big challenges into actionable objectives. 
+        As a former Peace Corps Volunteer, I know how to work with ambiguity, 
+        adapt to changes with grace, and communicate effectively to any team or audience. 
+        When my eyes need a break from the screen, I like to climb rocks, run trails, and read books. </p>
+
+      </body>
     </div>
   );
 }
