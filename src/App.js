b1d0b84333307e4b42bd1349084a070201b36f43
joegelay <joegelay@gmail.com>
2020-04-27 15:28:11 -0600

    center content, adjust max width of page

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import profilePicture from './images/profile-picture.jpg'
 function App() {
   return (
     <div className="App">
+      <div id="main-display">
+
       <body>
 
       <nav class="main-nav">
         <a class="main-nav-link" href="">Home</a>
-        <a class="main-nav-link" href="" href="">Projects</a>
-        <a class="main-nav-link" href="" href="">Resume</a>
-        <a class="main-nav-link" href="" href="">Writing</a>
-        <a class="main-nav-link" href="" href="">Reading</a>
+        <a class="main-nav-link" href="">Projects</a>
+        <a class="main-nav-link" href="">Resume</a>
+        <a class="main-nav-link" href="">Writing</a>
+        <a class="main-nav-link" href="">Reading</a>
       </nav>
 
       <header id="hi">Hi, I'm Joe.</header>
@@ -26,7 +28,7 @@ function App() {
         My background in marketing analytics has shaped me into being a detail-oriented thinker 
         with an eye for the big picture. 
         As a former Peace Corps Volunteer, I know how to work with ambiguity, 
-        adapt to changes with grace, and communicate effectively with any team or audience. 
+        adapt to changes with grace, and communicate effectively with any team. 
         <br></br> 
         <br></br> 
         When my eyes need a break from the screen, I like to climb rocks, run trails, and read books. </p>
@@ -36,6 +38,8 @@ function App() {
       </div>
 
       </body>
+
+      </div>
     </div>
   );
 }
