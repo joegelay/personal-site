4d358ed62969ad31655dd43b2367a5d912043dcb
joegelay <joegelay@gmail.com>
2020-04-27 12:00:31 -0600

    apply cropping and border to profile image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/reset.css';
 import './styles/App.css';
+import profilePicture from './images/profile-picture.jpg'
 
 function App() {
   return (
@@ -8,12 +9,14 @@ function App() {
       <body>
 
       <h1>Hi, I'm Joe.</h1>
-      <img src="./images/profile-picture.jpg" alt="Joe Gelay" />
+      <div id="image-cropper">
+        <img src={profilePicture} alt="Joe Gelay" id="profile-picture" />
+      </div>
       <p>I love to learn new things, solve problems, and connect people. 
         My background in marketing analytics has shaped me into being a detail-oriented thinker 
         with an eye for the big picture. 
         As a former Peace Corps Volunteer, I know how to work with ambiguity, 
-        adapt to changes with grace, and communicate effectively to any team or audience. 
+        adapt to changes with grace, and communicate effectively with any team or audience. 
         When my eyes need a break from the screen, I like to climb rocks, run trails, and read books. </p>
 
       </body>
