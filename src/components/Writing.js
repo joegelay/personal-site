0e1058773af31f1cc383abd03111499d85c311a1
joegelay <joegelay@gmail.com>
2020-04-28 10:30:25 -0600

    add medium articles to Writing page through pixelpoint widget

--- src/components/Writing.js
+++ src/components/Writing.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 
- 
+
 class Writing extends Component {
+  
+  componentDidMount() {
+    try {
+      const widget = document.getElementById("medium-widget");
+      if (!!widget) {
+        window.mediumWidget();
+      }
+    }
+    catch(e){
+      window.location.reload();
+    }
+  }
+  
   render() {
     return (
         <div className="content">
             <header id="content-header">What I've Written</header>
 
-
-
-           
+            <div id="medium-widget"></div>
         </div>
     );
   }
