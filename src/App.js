7b5b3779a82ff2dd7b31d24b51c1ce3a1e49512d
joegelay <joegelay@gmail.com>
2020-04-27 21:16:10 -0600

    add blowup styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
+import { Route, NavLink, HashRouter } from "react-router-dom";
 import './styles/reset.css';
 import './styles/App.css';
 import About from './components/About.js'
+import Projects from './components/Projects.js'
 
 function App() {
   return (
+    <HashRouter>
     <div className="App">
       <div id="main-display">
 
-        <nav className="main-nav">
+        {/* <nav className="main-nav">
           <a className="main-nav-link" href="/home.html">About</a>
           <a className="main-nav-link" href="/projects.html">Projects</a>
           <a className="main-nav-link" href="/resume.html">Resume</a>
           <a className="main-nav-link" href="/writing.html">Writing</a>
           <a className="main-nav-link" href="/reading.html">Reading</a>
+        </nav> */}
+        <nav className="main-nav">
+          <NavLink className="main-nav-link" exact to="/">About</NavLink>
+          <NavLink className="main-nav-link" to="/projects">Projects</NavLink>
+          <NavLink className="main-nav-link" to="/resume">Resume</NavLink>
+          <NavLink className="main-nav-link" to="/writing">Writing</NavLink>
+          <NavLink className="main-nav-link" to="/reading">Reading</NavLink>
         </nav>
 
-        <About />
+        <Route exact path="/" component={About}/>
+        <Route path="/projects" component={Projects}/>
 
       </div>
     </div>
+    </HashRouter>
   );
 }
 
