de9b012dea41526b852e5d73e07db388325a4e10
joegelay <joegelay@gmail.com>
2020-11-23 10:15:13 -0700

    add blog.js component, begin reworking medium feed page

---
+++ src/components/Blog.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+const Blog = () => {
+  const [mediumData, setMediumData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@joegelay`
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        setMediumData(response.items);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+};
