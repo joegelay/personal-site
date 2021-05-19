11884cace20b44fb25fe3b6ea45f1584964516be
joegelay <joegelay@gmail.com>
2021-05-19 12:06:30 -0600

    update production build

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,60 +1,90 @@
 import React, { Component } from 'react';
-import githubIcon from '../images/github.svg'
-import linkIcon from '../images/link.svg'
-import youtubeIcon from '../images/youtube.svg'
-import FLP from '../images/FineLinePhilosophy.png'
-import CLIJournalApp from '../images/CLIJournalApp.png'
-import Barterbook from '../images/Barterbook.png'
-import KindCard from '../images/KindCard.png'
+import githubIcon from '../images/github.svg';
+import linkIcon from '../images/link.svg';
+import youtubeIcon from '../images/youtube.svg';
+import FLP from '../images/FineLinePhilosophy.png';
+import CLIJournalApp from '../images/CLIJournalApp.png';
+import Barterbook from '../images/Barterbook.png';
+import KindCard from '../images/KindCard.png';
+import Billionaire from '../images/billionaire.png';
 
 const imageMap = {
-    'FineLinePhilosophy': FLP,
-    'CLIJournalApp': CLIJournalApp,
-    'Barterbook': Barterbook,
-    'KindCard': KindCard
-}
-  
+  FineLinePhilosophy: FLP,
+  CLIJournalApp: CLIJournalApp,
+  Barterbook: Barterbook,
+  KindCard: KindCard,
+  Billionaire: Billionaire,
+};
+
 export default class ProjectCard extends Component {
+  websiteLink = () => {
+    return (
+      <li>
+        <a
+          className="project-icon-a"
+          href={this.props.link}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img className="project-icon" src={linkIcon} alt="link-icon" />
+        </a>
+      </li>
+    );
+  };
 
-    checkLink = () => {
-        if (this.props.link) {
-            return (
-                <li>
-                    <a className="project-icon-a" href={this.props.link} rel="noopener noreferrer" target="_blank">
-                        <img className="project-icon" src={linkIcon} alt="link-icon" />
-                    </a>
-                </li>
-            );
-        }
-    };
+  gitHubLink = () => {
+    return (
+      <li>
+        <a
+          className="project-icon-a"
+          href={this.props.github}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img className="project-icon" src={githubIcon} alt="github-icon" />
+        </a>
+      </li>
+    );
+  };
 
-    render() {
-        return (
-            <div className="project-card">
-            <ul>
-                <li className="project-card-image">
-                    <a href={this.props.link} rel="noopener noreferrer" target="_blank">
-                        <img className="project-card-image" src={imageMap[this.props.image]} alt="Project Preview"/>
-                    </a>
-                </li>
-                <li className="project-card-title">{this.props.title}</li>
-                <li className="project-card-description">{this.props.description}</li>
-                <li className="project-card-stack">{this.props.stack}</li>
-                <div className="project-links">
-                    {this.checkLink()}
-                    <li>
-                        <a className="project-icon-a" href={this.props.github} rel="noopener noreferrer" target="_blank">
-                            <img className="project-icon" src={githubIcon} alt="github-icon" />
-                        </a>
-                    </li>
-                    <li>
-                        <a className="project-icon-a" href={this.props.youtube} rel="noopener noreferrer" target="_blank">
-                            <img className="project-icon" src={youtubeIcon} alt="github-icon" />
-                        </a>
-                    </li>
-                </div>
-            </ul>
-        </div>
-    )
+  youTubeLink = () => {
+    return (
+      <li>
+        <a
+          className="project-icon-a"
+          href={this.props.youtube}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img className="project-icon" src={youtubeIcon} alt="github-icon" />
+        </a>
+      </li>
+    );
+  };
+
+  render() {
+    return (
+      <div className="project-card">
+        <ul>
+          <li className="project-card-image">
+            <a href={this.props.link} rel="noopener noreferrer" target="_blank">
+              <img
+                className="project-card-image"
+                src={imageMap[this.props.image]}
+                alt="Project Preview"
+              />
+            </a>
+          </li>
+          <li className="project-card-title">{this.props.title}</li>
+          <li className="project-card-description">{this.props.description}</li>
+          <li className="project-card-stack">{this.props.stack}</li>
+          <div className="project-links">
+            {this.props.link ? this.websiteLink() : null}
+            {this.props.github ? this.gitHubLink() : null}
+            {this.props.youtube ? this.youTubeLink() : null}
+          </div>
+        </ul>
+      </div>
+    );
   }
 }
--- src/projectData.js
+++ src/projectData.js
@@ -1,38 +1,51 @@
 const projectData = [
-    {
-        title: 'KindCard',
-        image: 'KindCard',
-        description: "How much impact can a single act of kindness have? KindCard aims to track these interactions to show their true impact.",
-        stack: 'React.js, Leaflet.js, Node, Express.js, CSS, PostgreSQL',
-        github: 'https://github.com/joegelay/kindcard-frontend',
-        link: 'https://kindcardmap.com',
-        youtube: 'https://www.youtube.com/watch?v=UNF6YMRWQWQ'
-    },
-    {
-        title: 'Fine Line Philosophy',
-        image: 'FineLinePhilosophy',
-        description: "Fine Line Philosophy was created to parse out meaning, parallels, and contrasts of similar but often confused words or ideas.",
-        stack: 'Rails, PostgreSQL, Vanilla JS, HTML + CSS, ScrollMagic.js',
-        github: 'https://github.com/joegelay/fine-line-philosophy-frontend',
-        link: 'https://finelinephilosophy.com',
-        youtube: 'https://www.youtube.com/watch?v=Hp6zfkOzCyA'
-    },
-    {
-        title: 'CLI Journal App',
-        image: 'CLIJournalApp',
-        description: "A simple CLI-based journal app that is easy to use and fast to run.",
-        stack: 'Ruby, SQLite',
-        github: 'https://github.com/joegelay/cli_journal_app',
-        youtube: 'https://www.youtube.com/watch?v=G5__x6t5CQs'
-    },
-    {
-        title: 'Barterbook',
-        image: 'Barterbook',
-        description: "Barterbook is a community-based skill sharing app built on the idea that everyone has something to teach, and something they want to learn.",
-        stack: 'Rails, Vanilla JS, HTML + CSS, Bootstrap',
-        github: 'https://github.com/joegelay/Mod2Project_BarterBook',
-        youtube: 'https://www.youtube.com/watch?v=258CrIi6kJg'
-    },
-  ]
-  
-  export default projectData
+  {
+    title: 'KindCard',
+    image: 'KindCard',
+    description:
+      'How much impact can a single act of kindness have? KindCard aims to track these interactions to show their true impact.',
+    stack: 'React.js, Leaflet.js, Node, Express.js, CSS, PostgreSQL',
+    github: 'https://github.com/joegelay/kindcard-frontend',
+    link: 'https://kindcardmap.com',
+    youtube: 'https://www.youtube.com/watch?v=UNF6YMRWQWQ',
+  },
+  {
+    title: 'You Vs. A Billionaire',
+    image: 'Billionaire',
+    description:
+      'A single page app designed to spark awareness and conversation about both domestic and global wealth inequality, its impact on our lives, and the impact we can have on others.',
+    stack: 'React.js, AOS animation library, CSS',
+    github: 'https://github.com/joegelay/you-vs-a-billionaire',
+    link: 'https://youvsabillionaire.com',
+  },
+  {
+    title: 'Fine Line Philosophy',
+    image: 'FineLinePhilosophy',
+    description:
+      'Fine Line Philosophy was created to parse out meaning, parallels, and contrasts of similar but often confused words or ideas.',
+    stack: 'Rails, PostgreSQL, Vanilla JS, HTML + CSS, ScrollMagic.js',
+    github: 'https://github.com/joegelay/fine-line-philosophy-frontend',
+    link: 'https://finelinephilosophy.com',
+    youtube: 'https://www.youtube.com/watch?v=Hp6zfkOzCyA',
+  },
+  {
+    title: 'CLI Journal App',
+    image: 'CLIJournalApp',
+    description:
+      'A simple CLI-based journal app that is easy to use and fast to run.',
+    stack: 'Ruby, SQLite',
+    github: 'https://github.com/joegelay/cli_journal_app',
+    youtube: 'https://www.youtube.com/watch?v=G5__x6t5CQs',
+  },
+  {
+    title: 'Barterbook',
+    image: 'Barterbook',
+    description:
+      'Barterbook is a community-based skill sharing app built on the idea that everyone has something to teach, and something they want to learn.',
+    stack: 'Rails, Vanilla JS, HTML + CSS, Bootstrap',
+    github: 'https://github.com/joegelay/Mod2Project_BarterBook',
+    youtube: 'https://www.youtube.com/watch?v=258CrIi6kJg',
+  },
+];
+
+export default projectData;
