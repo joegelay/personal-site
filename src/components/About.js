ba2b1cb9c2e91f0bb59f86159d5e5584fdb03250
joegelay <joegelay@gmail.com>
2022-09-21 20:47:40 -0600

    update blurb

--- src/components/About.js
+++ src/components/About.js
@@ -1,52 +1,66 @@
-import React, { Component } from "react";
-import profilePicture from '../images/profile-picture.jpg'
-import emailIcon from '../images/gmail.svg'
-import linkedInIcon from '../images/linkedin.svg'
-import githubIcon from '../images/github.svg'
- 
+import React, { Component } from 'react';
+import profilePicture from '../images/profile-picture.jpg';
+import emailIcon from '../images/gmail.svg';
+import linkedInIcon from '../images/linkedin.svg';
+import githubIcon from '../images/github.svg';
+
 class About extends Component {
   render() {
     return (
-        <div className="content">
-            <header id="hi">Hi, I'm Joe.</header>
+      <div className='content'>
+        <header id='hi'>Hi, I'm Joe.</header>
 
-            <div id="blurb-and-image">
-
-            <p id="blurb">I'm a full stack developer with a deep love for learning, solving problems, and connecting people.
-            <br></br> 
-            <br></br> 
-            My background in marketing analytics has shaped me into being a detail-oriented thinker 
-            with an eye for the big picture. 
-            As a former Peace Corps Volunteer, I know how to work with ambiguity, 
-            adapt to changes with grace, and communicate effectively with any team. 
-            <br></br> 
-            <br></br> 
-            When my eyes need a break from the screen, you can find me climbing rocks, running trails, reading books, and petting dogs. </p>
-            <div id="image-cropper">
-            <img src={profilePicture} alt="Joe Gelay" id="profile-picture" />
-            </div>
-
-            </div>
-
-            <nav id="contact-nav">
-            <a className="contact-icon-a" href="mailto:joegelay@gmail.com?Subject=Hello!" target="_top">        
-                <img className="contact-icon" src={emailIcon} alt="email-icon"/>
-            </a>
-            <a className="contact-icon-a" href="https://www.linkedin.com/in/joegelay/" rel="noopener noreferrer" target="_blank">
-                <img className="contact-icon" src={linkedInIcon} alt="linkedin-icon"/>
-            </a>
-            <a className="contact-icon-a" href="https://github.com/joegelay" rel="noopener noreferrer" target="_blank">
-                <img className="contact-icon" src={githubIcon} alt="github-icon" />
-            </a>
-            </nav>
+        <div id='blurb-and-image'>
+          <p id='blurb'>
+            I'm a full stack developer with a deep love for learning, solving
+            problems, and connecting people.
+            <br></br>
+            <br></br>
+            As a former Peace Corps Volunteer, I know how to work with
+            ambiguity, adapt to changes with grace, and communicate effectively
+            with any team.
+            <br></br>
+            <br></br>
+            When my eyes need a break from the screen, you can find me climbing
+            rocks, running trails, reading books, and petting dogs.{' '}
+          </p>
+          <div id='image-cropper'>
+            <img src={profilePicture} alt='Joe Gelay' id='profile-picture' />
+          </div>
         </div>
-       
+
+        <nav id='contact-nav'>
+          <a
+            className='contact-icon-a'
+            href='mailto:joegelay@gmail.com?Subject=Hello!'
+            target='_top'
+          >
+            <img className='contact-icon' src={emailIcon} alt='email-icon' />
+          </a>
+          <a
+            className='contact-icon-a'
+            href='https://www.linkedin.com/in/joegelay/'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <img
+              className='contact-icon'
+              src={linkedInIcon}
+              alt='linkedin-icon'
+            />
+          </a>
+          <a
+            className='contact-icon-a'
+            href='https://github.com/joegelay'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <img className='contact-icon' src={githubIcon} alt='github-icon' />
+          </a>
+        </nav>
+      </div>
     );
   }
 }
- 
+
 export default About;
-
-
-
-
